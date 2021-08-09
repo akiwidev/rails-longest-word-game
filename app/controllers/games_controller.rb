50f787ff2ac25c179a08854c3127ad88bf5c0a2c
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-09 19:06:38 +0900

    finished api exercise

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,37 @@
 require 'open-uri'
+require 'json'
 
 class GamesController < ApplicationController
+  before_action :word_included_in_letters?, only: [:score]
+  before_action :valid_word?, only: [:score]
+
   def new
     @letters = ('a'..'z').to_a.sample(10)
   end
 
   def score
+    @message =
+      if @valid == false
+        "Sorry, but #{@word} does not seem to be a valid English word..."
+      elsif !@included
+        "Sorry, but #{@word} can't be built out of #{@letters}"
+      else
+        "Congratualtions! #{@word.capitalize} is a valid English word!"
+      end
+  end
+
+  private
+
+  def word_included_in_letters?
     @word = params[:word]
     @letters = params[:letters]
-    word_made_of_letters = @word.chars.all? do |letter|
-      @letters.include?(letter)
-    end
+    @included = @word.chars.all? { |letter| @letters.include?(letter) }
+  end
 
+  def valid_word?
+    @word = params[:word]
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
-    json = open(url).read
-    raise
-    # if the word can't be built out of the shown letters, return "Sorry, but {word} can't be built out of {letters}"
-    @result =
-      unless word_made_of_letters
-        "Sorry, but #{@word} can't be built out of #{@letters}"
-      end
-
-    # if the word isn't a valid English word, return "Sorry, but {word} does not seem to be a valid English word..."
-    # if the word is valid, return "Congratualtions! {word} is a valid English word!"
+    json = URI.open(url).read
+    @valid = JSON.parse(json)['found']
   end
 end
